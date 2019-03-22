aa4cedfa3e1f1aa14eddc53385fef13e6afd73a8
amilford87 <amilford87@gmail.com>
2019-03-22 21:31:17 +0000

    cleaning up some code and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :reviews
+
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, presence: true
+
 end
