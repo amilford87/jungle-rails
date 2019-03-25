229e6fbc7887233d6a8b8dc9c1b12281195f732d
amilford87 <amilford87@gmail.com>
2019-03-25 17:49:32 +0000

    refactored product tests to be more accurate

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,34 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    cat1 = Category.new name: 'Whatever'
-    product = cat1.products.new
-    product.name = 'product'
-    product.price = 999.00
-    product.quantity = 45
-
+    before (:each) do
+    @cat1 = Category.new name: 'Whatever'
+    @cat1.save
+    @product = @cat1.products.new
+    @product.save
+    @product.name = 'product'
+    @product.price = 999.00
+    @product.quantity = 45
+    @product.save
+  end
+  
     it 'is valid with valid attributes' do
-      expect(product).to be_valid
+      expect(@product).to be_valid
     end
 
     it 'is not valid without a name' do
-      product.name = nil
-      expect(product).to_not be_valid
+      @product.name = nil
+      @product.save
+      expect(@product.errors.full_messages.include? "Name can't be blank").to eql(true)
     end
 
     it 'is not valid without a price' do
-      product.price = nil
-      expect(product).to_not be_valid
+      @product2 = @cat1.products.new(name: 'product2', quantity: 45)
+      @product2.save
+      expect(@product2.errors.full_messages.include? "Price can't be blank").to eql(true)
     end
 
     it 'is not valid without a quantity' do
-      product.quantity = nil
-      expect(product).to_not be_valid
+      @product.quantity = nil
+      @product.save
+      expect(@product.errors.full_messages.include? "Quantity can't be blank").to eql(true)
     end
 
     it 'is not valid without a category' do
-      product.category = nil
-      expect(product).to_not be_valid
+      @product.category = nil
+      @product.save
+      expect(@product.errors.full_messages.include? "Category can't be blank").to eql(true)
     end
   end
 end
