48b13c36db176df7d9ad2952d68fb5f19cf12d6c
amilford87 <amilford87@gmail.com>
2019-03-26 18:35:10 +0000

    added new test to test that user can click on a product from the homepage and go into the product page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -17,12 +17,16 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see the product page from items selected" do
     visit root_path
-
+    find('article.id_1 a.pull-right').click
+    sleep(1.seconds)
     # commented out b/c it's for debugging only
-    # save_and_open_screenshot
+    # save_screenshot
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to  have_content("64.99")
+    expect(page).to  have_css('article.product-detail')
   end
+
+
 end
